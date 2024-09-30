28b149a171ed5ac579fccb47b6ac1f5014185f5a
Dmitry Subbotin <sub-d-a@mail.ru>
2024-09-30 17:17:49 +0700

    An option of reverse solution added. It uses two-dimensional array.

--- reverse/Reverse.java
+++ reverse/Reverse.java
@@ -15,28 +15,61 @@ public class Reverse {
         // Scanner lineScanner = new Scanner(input);
 
         Scanner lineScanner = new Scanner(System.in);
-        
-        Stack<Stack<Integer>> valsInStacks = new Stack<>();
 
-        while(lineScanner.hasNextLine()) {
-            Scanner intScanner = new Scanner(lineScanner.nextLine());
+        /*
+         * Option 1 : Using two-dimensional array. Works slow.
+        */
 
-            valsInStacks.push(new Stack<Integer>());
-            while(intScanner.hasNextInt()) {
-                valsInStacks.peek().push(intScanner.nextInt());
+        int[][] valsmtrx = new int[0][0];
+        for(int i = 0; lineScanner.hasNextLine(); i++) {
+            if(i >= valsmtrx.length) {
+                valsmtrx = Arrays.copyOf(valsmtrx, valsmtrx.length+1);
+                valsmtrx[i] = new int[0];
             }
 
+            Scanner intScanner = new Scanner(lineScanner.nextLine());
+            for(int j = 0; intScanner.hasNextLine(); j++) {
+                if (j >= valsmtrx[i].length) {
+                    valsmtrx[i] = Arrays.copyOf(valsmtrx[i], valsmtrx[i].length+1);
+                }
+                valsmtrx[i][j] = intScanner.nextInt();
+            }
             intScanner.close();
         }
         lineScanner.close();
 
-        while(!valsInStacks.isEmpty()) {
-            Stack<Integer> vals = valsInStacks.pop();
-            while(!vals.isEmpty()) {
-                Integer val = vals.pop();
-                System.out.print(val + " ");
+        for(int i = valsmtrx.length-1; i >= 0; i--) {
+            for(int j = valsmtrx[i].length-1; j >= 0; j--) {
+                System.out.print(valsmtrx[i][j] + " ");
             }
             System.out.println();
         }
+
+        /*
+         * Option 2 : Using collections framework. Works much faster!
+        */
+        
+        // Stack<Stack<Integer>> valsInStacks = new Stack<>();
+
+        // while(lineScanner.hasNextLine()) {
+        //     Scanner intScanner = new Scanner(lineScanner.nextLine());
+
+        //     valsInStacks.push(new Stack<Integer>());
+        //     while(intScanner.hasNextInt()) {
+        //         valsInStacks.peek().push(intScanner.nextInt());
+        //     }
+
+        //     intScanner.close();
+        // }
+        // lineScanner.close();
+
+        // while(!valsInStacks.isEmpty()) {
+        //     Stack<Integer> vals = valsInStacks.pop();
+        //     while(!vals.isEmpty()) {
+        //         Integer val = vals.pop();
+        //         System.out.print(val + " ");
+        //     }
+        //     System.out.println();
+        // }
     }
 }
